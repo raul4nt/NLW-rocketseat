ac6239bd0a0374eb9f7d782fe3a8502f75d1ec71
Raul Silveira <raulantsilveira@gmail.com>
2024-07-14 20:53:30 -0300

    feat: Implementing link finder

--- init/schema.sql
+++ init/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS trips (
+CREATE TABLE IF NOT EXISTS 'trips' (
     id TEXT PRIMARY KEY,
     destination TEXT NOT NULL,
     start_date DATETIME,
@@ -8,17 +8,35 @@ CREATE TABLE IF NOT EXISTS trips (
     status INTEGER -- 1 para verdadeiro (true), 0 para falso (false)
 );
 
-CREATE TABLE IF NOT EXISTS emails_to_invite (
+CREATE TABLE IF NOT EXISTS 'emails_to_invite' (
     id TEXT PRIMARY KEY,
     trip_id TEXT,
     email TEXT NOT NULL,
     FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
 
-CREATE TABLE IF NOT EXISTS links (
+CREATE TABLE IF NOT EXISTS 'links' (
     id TEXT PRIMARY KEY,
     trip_id TEXT,
     link TEXT NOT NULL,
     title TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
+
+CREATE TABLE IF NOT EXISTS 'participants' (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT NOT NULL,
+    emails_to_invite_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    is_confirmed INTEGER, -- 1 para verdadeiro (true), 0 para falso (false)
+    FOREIGN KEY (trip_id) REFERENCES trips(id),
+    FOREIGN KEY (emails_to_invite_id) REFERENCES emails_to_invite(id)
+);
+
+CREATE TABLE IF NOT EXISTS 'activities' (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    occurs_at DATETIME,
     FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -6,7 +6,9 @@ trips_routes_bp = Blueprint("trip_routes", __name__)
 from src.controllers.trip_creator import TripCreator
 from src.controllers.trip_finder import TripFinder
 from src.controllers.trip_confirmer import TripConfirmer
+
 from src.controllers.link_creator import LinkCreator
+from src.controllers.link_finder import LinkFinder
 
 #Importação de Repositorios
 
@@ -49,11 +51,23 @@ def confirm_trip(tripId):
 
     return jsonify(response["body"]), response["status_code"]
 
-@trips_routes_bp.route("/trips/<tripId>/confirm", methods=["POST"])
+@trips_routes_bp.route("/trips/<tripId>/links", methods=["POST"])
 def create_trip_link(tripId):
     conn = db_connection_handler.get_connection()
     link_repository = LinksRepository(conn)
     controller = LinkCreator(link_repository)
 
     response = controller.create(request.json, tripId)
+
     return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<tripId>/links", methods=["GET"])
+def find_trip_link(tripId):
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+    controller = LinkFinder(link_repository)
+
+    response = controller.find(tripId)
+
+    return jsonify(response["body"]), response["status_code"]
+
